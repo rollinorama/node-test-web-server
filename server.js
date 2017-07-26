836de09f0e4f5469e45ea7edb25cde4ca52a588d
Bruno Rollin <bruno.rollin@esauce.com.br>
2017-07-25 23:15:48 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+var app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.originalUrl}`;
+
+    console.log(log);
+    fs.appendFile('server.log', `${log}\n`, err => {
+        if (err)
+            console.log('Unable to save on logger');
+    });
+    next();
+})
+// app.use((req, res, next) => res.render('maintenance.hbs'));
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    // res.send('Hello Express!');
+    res.render('home.hbs', {
+        pageTitle: 'Homepage',
+        welcomeMessage: 'Welcome home King Diamond!!!',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
